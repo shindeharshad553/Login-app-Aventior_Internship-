9bdb1edb2fdd0c2440cb8c00f352d6568d59982e
shindeharshad553 <shindeharshad553@gmail.com>
2025-07-31 16:21:48 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -7,24 +7,6 @@ import AppRoutes from './AppRoutes.js'
 function App() {
   const [isLogedInOrNot,setIsLogedInOrNot] = useState(false);
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
-
     // specifying the routes to be navigate 
 
       <BrowserRouter>
